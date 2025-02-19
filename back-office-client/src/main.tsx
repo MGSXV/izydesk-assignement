355ee447c38094c8e7feeea1e44d0bf1caf7b8a2
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-20 00:38:49 +0100

    Wrap App component with BrowserRouter and ThemeProvider, and include AuthProvider for authentication context

--- back-office-client/src/main.tsx
+++ back-office-client/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@/components/theme-provider.tsx'
+import { AuthProvider } from '@/context/'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+				<AuthProvider>
+					<App />
+				</AuthProvider>
+			</ThemeProvider>
+		</BrowserRouter>
 	</StrictMode>,
 )
