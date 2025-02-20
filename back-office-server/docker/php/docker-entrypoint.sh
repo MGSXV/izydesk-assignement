6f362fc56702ec25c9624003eff20b80cf462702
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-20 21:54:30 +0100

    initial commit

---
+++ back-office-server/docker/php/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# Check if the project is already initialized
+if [ ! -f composer.json ]; then
+    composer create-project symfony/skeleton:"6.4.*" .
+    composer require symfony/orm-pack
+    composer require symfony/maker-bundle --dev
+    composer require lexik/jwt-authentication-bundle
+    composer require symfony/security-bundle
+fi
+
+# First arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- php-fpm "$@"
+fi
+
+exec "$@"
