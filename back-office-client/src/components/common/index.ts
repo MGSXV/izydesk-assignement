269919e8258964d4d1b5f1bdab7c4c1f7ab5ab55
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-21 19:33:55 +0100

    Add LayoutAuth and Navbar exports to common components

--- back-office-client/src/components/common/index.ts
+++ back-office-client/src/components/common/index.ts
@@ -1,3 +1,5 @@
 export { default as NotFound } from './NotFound';
 export { default as Layout } from './Layout';
 export { default as RequireAuth } from './RequireAuth';
+export { default as LayoutAuth } from './LayoutAuth';
+export { Navbar } from './Navigation'
