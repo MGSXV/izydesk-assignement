5e6e84a55e944b9b66caa76fb32498e6b67f66d6
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-22 02:05:12 +0100

    Wrap AuthOutlet with CategoryProvider in LayoutAuth component

--- back-office-client/src/components/common/LayoutAuth.tsx
+++ back-office-client/src/components/common/LayoutAuth.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom"
 import { SidebarProvider } from "@/components/ui/sidebar"
 import AuthOutlet from "./AuthOutlet"
+import { CategoryProvider } from "@/context"
 
 const LayoutAuth = () => {
 	return (
 		<SidebarProvider>
-			<AuthOutlet>
-				<Outlet />
-			</AuthOutlet>
+			<CategoryProvider>
+				<AuthOutlet>
+					<Outlet />
+				</AuthOutlet>
+			</CategoryProvider>
 		</SidebarProvider>
 	)
 }
