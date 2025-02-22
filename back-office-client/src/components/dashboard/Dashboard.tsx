06993b29796984bb22720aca5a03b0699035a736
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-22 02:13:37 +0100

    Refactor Dashboard component layout and add state management for dialog visibility

--- back-office-client/src/components/dashboard/Dashboard.tsx
+++ back-office-client/src/components/dashboard/Dashboard.tsx
@@ -1,9 +1,22 @@
+// import { AddDepotDialog, DepotCards } from "@/components/depot";
+import { Fragment, useState } from "react";
+
 const Dashboard = () => {
+
+	const [isOpen, setIsOpen] = useState(false)
+	const handleOpen = () => setIsOpen(!isOpen)
+
 	return (
-		<div>
-			<h1>Dashboard</h1>
-		</div>
-	)
+		<Fragment>
+			<div className="w-full h-fit flex flex-col gap-3">
+				<h2 className="text-3xl font-bold tracking-tight">
+					Dashboard
+				</h2>
+				{/* <DepotCards onclick={handleOpen} /> */}
+			</div>
+			{/* <AddDepotDialog isOpen={isOpen} onOpenChange={handleOpen} /> */}
+		</Fragment>
+	);
 }
 
-export default Dashboard
+export default Dashboard;
