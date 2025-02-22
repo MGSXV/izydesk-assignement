d3b713689487854e0843ce5f446a1ed5f744b125
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-22 01:50:45 +0100

    Refactor NavCategories component to streamline imports and enhance category handling

--- back-office-client/src/components/nav-categories.tsx
+++ back-office-client/src/components/nav-categories.tsx
@@ -1,11 +1,11 @@
 import { ICategory } from "@/types"
 import { Fragment, useRef, useState } from "react"
-import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuAction, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem, useSidebar } from "@/components/ui/sidebar"
+import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuAction, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/components/ui/sidebar"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { ChevronRight, EditIcon, MoreHorizontal, PlusIcon, Trash2, ViewIcon } from "lucide-react"
+import { EditIcon, MoreHorizontal, PlusIcon, Trash2, ViewIcon } from "lucide-react"
 import { DropdownMenu, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger, DropdownMenuContent } from "@/components/ui/dropdown-menu"
-// import { AddCategory, DeleteCategory, EditCategory } from "./category"
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
+import { AddCategory, /*DeleteCategory, EditCategory */} from "./category"
+import { Collapsible } from "@/components/ui/collapsible"
 
 export const NavCategories = ({ categories }: { categories: ICategory[] }) => {
 
@@ -15,8 +15,9 @@ export const NavCategories = ({ categories }: { categories: ICategory[] }) => {
 	const [isDeletDialogOpen, setIsDeleteDialogOpen] = useState(false)
 	const handleOpen = () => setIsAddDialogOpen(!isAddDialogOpen)
 
+	console.log('---->', categories)
+
 	const selectedCategoryId = useRef("")
-	const parentID = useRef("")
 	const selectedCategory = useRef<ICategory | null >(null)
 
 	const handleEditDialog = (category: ICategory) => {
@@ -50,7 +51,7 @@ export const NavCategories = ({ categories }: { categories: ICategory[] }) => {
 									<SidebarMenuButton asChild>
 										<a href={`/category/${item.id}`}>
 											<Avatar className="h-6 w-6 rounded-lg">
-												<AvatarImage src={item.image_url} />
+												<AvatarImage src={item.avatar} />
 												<AvatarFallback className="rounded-lg uppercase">
 													{`${item.name[0]}${item.name[1]}`}
 												</AvatarFallback>
@@ -81,7 +82,6 @@ export const NavCategories = ({ categories }: { categories: ICategory[] }) => {
 											<DropdownMenuSeparator />
 											<DropdownMenuItem className="cursor-pointer"
 												onClick={() => {
-													parentID.current = ""
 													handleDeleteDialogOpen(item.id)
 												}}>
 												<Trash2 className="text-muted-foreground" />
@@ -89,87 +89,12 @@ export const NavCategories = ({ categories }: { categories: ICategory[] }) => {
 											</DropdownMenuItem>
 										</DropdownMenuContent>
 									</DropdownMenu>
-									<CollapsibleTrigger asChild className="right-1">
-										<SidebarMenuAction className="data-[state=open]:rotate-90">
-											<ChevronRight />
-											<span className="sr-only">Toggle</span>
-										</SidebarMenuAction>
-									</CollapsibleTrigger>
-									<CollapsibleContent>
-										<SidebarMenuSub>
-											{item.childCategories && item.childCategories.length > 0 && (
-												<Fragment>
-													{item.childCategories.map(child => (
-														<SidebarMenuSubItem key={child.id}>
-															<SidebarMenuSubButton asChild>
-															<a href={`category/${child.id}`}>
-																<Avatar className="h-6 w-6 rounded-lg">
-																	<AvatarImage src={child.image_url} />
-																	<AvatarFallback className="rounded-lg uppercase">
-																		{`${child.name[0]}${child.name[1]}`}
-																	</AvatarFallback>
-																</Avatar>
-																<span>{child.name}</span>
-															</a>
-															</SidebarMenuSubButton>
-															<DropdownMenu>
-																<DropdownMenuTrigger asChild>
-																	<SidebarMenuAction showOnHover>
-																		<MoreHorizontal />
-																		<span className="sr-only">More</span>
-																	</SidebarMenuAction>
-																</DropdownMenuTrigger>
-																<DropdownMenuContent className="w-48" side={isMobile ? "bottom" : "right"}
-																	align={isMobile ? "end" : "start"}>
-																		<DropdownMenuItem>
-																			<ViewIcon className="text-muted-foreground" />
-																			<a href={`/category/${child.id}`}>
-																				<span>View Subcategory</span>
-																			</a>
-																		</DropdownMenuItem>
-																		<DropdownMenuItem className="cursor-pointer"
-																			onClick={() => {
-																				parentID.current = item.id
-																				handleEditDialog(child)
-																			}}>
-																			<EditIcon className="text-muted-foreground" />
-																			<span>Edit Category</span>
-																		</DropdownMenuItem>
-																		<DropdownMenuSeparator />
-																		<DropdownMenuItem className="cursor-pointer"
-																			onClick={() => {
-																				parentID.current = item.id
-																				handleDeleteDialogOpen(child.id)
-																			}}>
-																			<Trash2 className="text-muted-foreground" />
-																			<span>Delete Catgory</span>
-																		</DropdownMenuItem>
-																</DropdownMenuContent>
-															</DropdownMenu>
-														</SidebarMenuSubItem>
-													))}
-												</Fragment>
-											)}
-											<SidebarMenuSubItem>
-												<SidebarMenuSubButton asChild onClick={() => {
-													parentID.current = item.id
-													handleOpen()
-												}} className="cursor-pointer">
-												<div>
-													<PlusIcon className="text-muted-foreground size-20" />
-													<span>Add Subcategory</span>
-												</div>
-												</SidebarMenuSubButton>
-											</SidebarMenuSubItem>
-										</SidebarMenuSub>
-									</CollapsibleContent>
 								</SidebarMenuItem>
 							</Collapsible>
 						))
 					}
 					<SidebarMenuItem>
 						<SidebarMenuButton asChild onClick={() => {
-							parentID.current = ""
 							handleOpen()
 						}} className="cursor-pointer">
 							<div>
@@ -180,12 +105,12 @@ export const NavCategories = ({ categories }: { categories: ICategory[] }) => {
 					</SidebarMenuItem>
 				</SidebarMenu>
 			</SidebarGroup>
-			{/* <AddCategory isOpen={isAddDialogOpen} onOpenChange={handleOpen} parent_category={parentID.current} />
-			{selectedCategoryId.current &&
-				<DeleteCategory isOpen={isDeletDialogOpen} parent_id={parentID.current}
+			<AddCategory isOpen={isAddDialogOpen} onOpenChange={handleOpen} />
+			{/* {selectedCategoryId.current &&
+				<DeleteCategory isOpen={isDeletDialogOpen}
 					onOpenChange={setIsDeleteDialogOpen} id={selectedCategoryId.current} />}
 			{selectedCategory.current &&
-				<EditCategory isOpen={isEditDialogOpen} parent_id={parentID.current}
+				<EditCategory isOpen={isEditDialogOpen}
 					category={selectedCategory.current} onOpenChange={setIsEditDialogOpen} />} */}
 		</Fragment>
 	)
