eac375ff7ea00488041e7cdbe6d57d32c61cd2bf
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-22 19:38:24 +0100

    Refactor ProductCard component to integrate ImageViewer and PriceFormat for improved product display

--- back-office-client/src/components/product/product-card.tsx
+++ back-office-client/src/components/product/product-card.tsx
@@ -1,35 +1,37 @@
-import { Card, CardContent,/* CardHeader, CardTitle*/ } from "@/components/ui/card"
 import { IProduct } from "@/types"
 import defaultProduct from "@/assets/default-category.png"
-import { Button } from "../ui/button"
 import { useNavigate } from "react-router-dom"
+import { Button } from "@/components/ui/button";
+import ImageViewer_Basic from "@/components/ui/ImageViewer";
+import PriceFormat_Basic from "@/components/ui/PriceFormat";
 
 export const ProductCard = ({ product }: { product: IProduct }) => {
 
 	const navigator = useNavigate()
 
 	return (
-		<Card className="cursor-pointer flex flex-col justify-center items-start pt-6">
-			<CardContent className="flex w-full items-center justify-center flex-1">
-				<div className="relative flex flex-col w-full h-full max-w-xs overflow-hidden rounded-lg">
-					<div className="flex items-center justify-center w-full aspect-w-1 aspect-h-1 rounded-xl overflow-hidden mb-5">
-						<img className="object-cover w-full" src={product.avatar || defaultProduct} alt="product image" />
-					</div>
-					<div className="flex flex-col flex-1 gap-y-4">
-						<div className="flex flex-col gap-y-2">
-							<a href={`/category/${product.id}`}>
-								<h5 className="text-lg lg:text-xl font-bold tracking-tight truncate">
-								{product.name}
-								</h5>
-							</a>
-							<p className="text-base truncate">
-								{product.description || ""}
-							</p>
-						</div>
-						<Button onClick={() => navigator(`/product/${product.id}`)} variant="outline" >View product</Button>
-					</div>
+		<div className="flex w-[350px] flex-col gap-4 rounded-lg border p-2">
+			<div className="relative w-full">
+				<div className="absolute top-2 left-2 z-10 w-fit rounded-lg bg-primary/80 p-2">
+					<p className="text-xs font-semibold">20% OFF</p>
 				</div>
-			</CardContent>
-		</Card>
-	)
+				<ImageViewer_Basic imageUrl={defaultProduct} />
+			</div>
+
+			<div>
+				<p className="text-lg font-semibold">{product.name}</p>
+				<p className="text-muted-foreground text-sm">{product.description}</p>
+			</div>
+
+			<PriceFormat_Basic
+				prefix="$"
+				value={98.96}
+				className="text-2xl font-semibold"
+			/>
+
+			<div className="flex flex-row gap-4">
+				<Button onClick={() => navigator(`/product/${product.id}`)} variant="outline" >View product</Button>
+			</div>
+		</div>
+	);
 }
