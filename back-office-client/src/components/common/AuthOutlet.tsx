c146d638cfc002f83f223c8df919d6f0f57ed9d6
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-22 02:05:40 +0100

    Refactor AuthOutlet to use useAxiosPrivate for API requests and improve error handling

--- back-office-client/src/components/common/AuthOutlet.tsx
+++ back-office-client/src/components/common/AuthOutlet.tsx
@@ -3,24 +3,24 @@ import { Navbar } from ".";
 import { useCategory } from "@/context";
 import { useToast } from "@/hooks";
 import { AppSidebar } from "@/components/app-sidebar";
-import { axios } from "@/config/api"
+import { useAxiosPrivate } from "@/config/api"
 
 const AuthOutlet = ({ children }: { children: React.ReactNode }) => {
 
 	const { setCategories } = useCategory()
 	const { toast } = useToast()
-
+	const axios_private = useAxiosPrivate()
 
 	useEffect(() => {
 
-		axios.get("/api/categories").then((res: any) => {
+		axios_private.get("/api/categories").then((res: any) => {
 			if (res && res.data && res.status && res.status === 200) {
 				setCategories([...res.data])
 			}
 		}).catch((error: any) => {
 			toast({
 				title: "Error",
-				description: error?.response?.data?.error || 'An error occurred',
+				description: error?.message || 'An error occurred',
 				variant: "destructive",
 			})
 		})
