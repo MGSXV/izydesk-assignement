aacaa00efdceb0245b6b66654e215cfa2d2558ed
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-23 13:43:33 +0000

    Remove StrictMode wrapper from main entry point for simplified rendering

--- back-office-client/src/main.tsx
+++ back-office-client/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -7,13 +6,11 @@ import { ThemeProvider } from '@/components/theme-provider.tsx'
 import { AuthProvider } from '@/context/'
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<BrowserRouter>
-			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-				<AuthProvider>
-					<App />
-				</AuthProvider>
-			</ThemeProvider>
-		</BrowserRouter>
-	</StrictMode>,
+	<BrowserRouter>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</ThemeProvider>
+	</BrowserRouter>
 )
