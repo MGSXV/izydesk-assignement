ecb41f6c30fa920ae15aea259b5732a11c178b2e
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-23 22:08:59 +0000

    Update ProductCard to use random images and improve layout

--- ecommerce-client/src/components/common/product-card.tsx
+++ ecommerce-client/src/components/common/product-card.tsx
@@ -2,28 +2,29 @@ import ImageViewer from "@/components/commerce-ui/image-viewer-basic";
 import PriceFormat from "@/components/commerce-ui/price-format-basic";
 import { Button } from "@/components/ui/button";
 import { IProduct } from "@/types";
-
-const IMAGE_URL =
-	"https://raw.githubusercontent.com/stackzero-labs/ui/refs/heads/main/public/placeholders/headphone-1.jpg";
+import imag1 from "@/assets/bright-red-purse-with-gold.jpg"
+import imag2 from "@/assets/gold-zipper-on-black-fashion-backpack.jpg"
+import imag3 from "@/assets/modern-time-pieces.jpg"
+import imag4 from "@/assets/wood-leather-watches.jpg"
+import { getRandomInt } from "@/lib/utils";
 
 function ProductCard({ product }: { product: IProduct}) {
 
+	const images = [imag1, imag2, imag3, imag4, "https://raw.githubusercontent.com/stackzero-labs/ui/refs/heads/main/public/placeholders/headphone-1.jpg"];
+	const randomImage = images[getRandomInt(0, images.length - 1)];
+
 	return (
-		<div className="flex w-[350px] flex-col gap-4 rounded-lg border p-2">
-			<div className="relative w-full">
-				<ImageViewer imageUrl={product.avatar || IMAGE_URL} />
-			</div>
-
-			<div>
-				<p className="text-lg font-semibold">{product.name}</p>
-				<p className="text-muted-foreground text-sm">Shipped in 2-3 days</p>
-			</div>
-
-			<PriceFormat
-				prefix="$"
-				value={product.price}
-				className="text-2xl font-semibold"
-			/>
+		<div className="flex w-xs flex-col gap-4 rounded-lg border p-2">
+			<a href={`/product/${product.id}`}>
+				<div className="relative w-full">
+					<ImageViewer imageUrl={product.avatar || randomImage} />
+				</div>
+				<div>
+					<p className="text-lg font-semibold">{product.name}</p>
+					<p className="text-muted-foreground text-sm">Shipped in 2-3 days</p>
+				</div>
+				<PriceFormat prefix="$" value={product.price} className="text-2xl font-semibold"/>
+			</a>
 
 			<div className="flex flex-row gap-4">
 				<Button variant="outline">Add to cart</Button>
