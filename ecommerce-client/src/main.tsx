a58e95e9102a4c858d0fd003c76f59ff5c2469cd
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-23 16:01:23 +0000

    Enhance styling and integrate ThemeProvider with routing support

--- ecommerce-client/src/main.tsx
+++ ecommerce-client/src/main.tsx
@@ -1,7 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from '@/components/theme-provider.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-	<App />
+
+	<BrowserRouter>
+		<ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+			<App />
+		</ThemeProvider>
+	</BrowserRouter>
 )
