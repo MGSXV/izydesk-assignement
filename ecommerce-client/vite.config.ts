2399ac6e88e960b12ad9a68558aaaf6f9a7bc374
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-23 15:34:08 +0000

    Add autoprefixer to PostCSS configuration for improved CSS compatibility

--- ecommerce-client/vite.config.ts
+++ ecommerce-client/vite.config.ts
@@ -3,6 +3,7 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,7 +16,7 @@ export default defineConfig({
 	},
 	css: {
 		postcss: {
-			plugins: [tailwindcss(), ]
+			plugins: [tailwindcss(), autoprefixer()]
 		}
 	},
 	preview: {
