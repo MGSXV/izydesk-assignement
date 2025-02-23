64bc0a6347a2805f894c5d36ddb7ee80882115d9
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-23 19:21:26 +0000

    Add Header and Footer components to Layout, fetch categories on mount

--- ecommerce-client/src/components/common/Layout.tsx
+++ ecommerce-client/src/components/common/Layout.tsx
@@ -1,12 +1,40 @@
 import { Outlet } from "react-router-dom"
-import { Toaster } from "@/components/ui/sonner"
+import { Footer, Header } from "@/components/sections"
+import { axios } from "@/config/api"
+import { useEffect } from "react"
+import { useCategory } from "@/context/CategoryProvider"
+import { Toaster } from "@/components/ui/toaster"
+import { useToast } from "@/hooks"
 
 const Layout = () => {
 
+	const { setCategories } = useCategory()
+	const { toast } = useToast()
+
+	const fetchCategories = async () => {
+		axios.get('/api/categories').then((res: any) => {
+			if (res && res.data && res.status && res.status === 200) {
+				setCategories([...res.data])
+			}
+		}).catch((error: any) => {
+			toast({
+				title: 'Error',
+				description: error?.message || 'An error occurred',
+				variant: 'destructive'
+			})
+		})
+	}
+
+	useEffect(() => {
+		fetchCategories()
+	}, [])
+
 	return (
 		<>
-			<Toaster />
+			<Header />
 			<Outlet />
+			<Footer />
+			<Toaster />
 		</>
 	)
 }
