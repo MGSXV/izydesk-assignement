c8d2826acf80813fa1126647d20bf8d3fd9b9780
Soufiane Elkhamlichi <05soufiane@gmail.com>
2025-02-23 13:48:35 +0000

    Add loading state to RequireAuth component for improved user experience

--- back-office-client/src/components/common/RequireAuth.tsx
+++ back-office-client/src/components/common/RequireAuth.tsx
@@ -1,10 +1,21 @@
 import { useAuth } from "@/hooks"
+import { useEffect, useState } from "react"
 import { Navigate, Outlet, useLocation } from "react-router-dom"
 
 const RequireAuth = () => {
 
 	const { user } = useAuth()
 	const location = useLocation()
+	const [isLoading, setIsLoading] = useState(true)
+
+	useEffect(() => {
+		if (user) {
+			setIsLoading(false)
+		}
+	}, [user])
+	if (isLoading) {
+		return <div>Loading..</div>
+	}
 	if (!user && location.pathname !== '/auth') {
 		return <Navigate to="/auth" />
 	}
